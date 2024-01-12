f1b1b5e7202311ac6540160258d22830abb5d30d
unknown <21522315@gm.uit.edu.vn>
2024-01-12 18:41:06 +0700

    fetch API

--- js/story.js
+++ js/story.js
@@ -1,242 +1,90 @@
-﻿// Thêm class vào body
-document.body.classList.add('custom-font');
+﻿const tagItems = document.querySelectorAll('.tag')
+let output = document.querySelector('#output-kanji-ne')
+let btn = document.querySelector('#create')
+let output_frame = document.querySelector(".output")   
+let storyType = 'Normal'
+tagItems.forEach((item) => {
+    item.addEventListener('click', function () {
+        document
+            .querySelector('.tag.active')
+            ?.classList.remove('active')
+        this.classList.add('active')
+        storyType = this.innerText
+    })
+})
 
-
-const KANJI = [
-    {
-        kanji: '⼀',
-        stroke: '1',
-        mean: ['one', 'horizontal stroke'],
-        reading: 'いち'
-    },
-    {
-        kanji: '⼁',
-        stroke: '1',
-        mean: ['vertical stroke'],
-        reading: 'たてぼう'
-    },
-    { kanji: '⼂', stroke: '1', mean: ['dot'], reading: 'てん' },
-    {
-        kanji: '⼃',
-        stroke: '1',
-        mean: ['diagonal sweeping stroke'],
-        reading: 'の'
-    },
-    { kanji: '⼄', stroke: '1', mean: ['the second'], reading: 'おつ' },
-    { kanji: '⺃', stroke: '1', mean: ['the second'], reading: 'おつ' },
-    { kanji: '⼇', stroke: '2', mean: ['lid', 'top'], reading: 'なべぶた' },
-    { kanji: '⼈', stroke: '2', mean: ['person'], reading: 'ひと' },
-    {
-        kanji: '⺅',
-        stroke: '2',
-        mean: ['person'],
-        reading: 'にんべん',
-        other_reading: 'へん'
-    },
-    {
-        kanji: '𠆢',
-        stroke: '2',
-        mean: ['person'],
-        reading: 'ひとやね',
-        other_reading: 'かんむり'
-    },
-    {
-        kanji: '⼉',
-        stroke: '2',
-        mean: ['human legs'],
-        reading: 'ひとあし',
-        other_reading: 'あし'
-    },
-    { kanji: '⼊', stroke: '2', mean: ['to enter'], reading: 'いる' },
-    { kanji: '⼋', stroke: '2', mean: ['eight'], reading: 'はち' },
-    {
-        kanji: '⼌',
-        stroke: '2',
-        mean: ['to enclose'],
-        reading: 'けいがまえ',
-        other_reading: 'かまえ, けいがまえ'
-    },
-    {
-        kanji: '⼍',
-        stroke: '2',
-        mean: ['cover', 'crown'],
-        reading: 'わかんむり',
-        other_reading: 'かんむり'
-    },
-    {
-        kanji: '⼎',
-        stroke: '2',
-        mean: ['ice'],
-        reading: 'にすい',
-        other_reading: 'へん'
-    },
-    {
-        kanji: '⼏',
-        stroke: '2',
-        mean: ['table'],
-        reading: 'きにょう',
-        other_reading: 'つくり'
-    },
-    {
-        kanji: '⺇',
-        stroke: '2',
-        mean: ['wind'],
-        reading: 'かぜかんむり',
-        other_reading: 'かんむり'
-    },
-    {
-        kanji: '⼐',
-        stroke: '2',
-        mean: ['container', 'open box'],
-        reading: 'かんにょう',
-        other_reading: 'にょう'
-    },
-    { kanji: '⼑', stroke: '2', mean: ['knife', 'sword'], reading: 'かたな' },
-    {
-        kanji: '⺉',
-        stroke: '2',
-        mean: ['knife', 'sword'],
-        reading: 'りっとう',
-        other_reading: 'つくり'
-    },
-    { kanji: '⼒', stroke: '2', mean: ['power'], reading: 'ちから' },
-    {
-        kanji: '⼓',
-        stroke: '2',
-        mean: ['to wrap'],
-        reading: 'つつみがまえ',
-        other_reading: 'かまえ, つつみがまえ'
-    },
-    {
-        kanji: '⼔',
-        stroke: '2',
-        mean: ['spoon'],
-        reading: 'さじ',
-        other_reading: 'つくり'
-    },
-    {
-        kanji: '⼕',
-        stroke: '2',
-        mean: ['box'],
-        reading: 'はこがまえ',
-        other_reading: 'かまえ, はこがまえ'
-    },
-    {
-        kanji: '⼖',
-        stroke: '2',
-        mean: ['to conceal', 'hide'],
-        reading: 'かくしがまえ',
-        other_reading: 'かまえ, はこがまえ'
-    },
-    { kanji: '⼗', stroke: '2', mean: ['ten'], reading: 'じゅう' },
-    {
-        kanji: '⼼',
-        stroke: '4',
-        mean: ['heart', 'mind', 'spirit'],
-        reading: 'こころ'
-    },
-    {
-        kanji: '⺗',
-        stroke: '4',
-        mean: ['heart', 'mind', 'spirit'],
-        reading: 'したごころ',
-        other_reading: 'あし'
-    },
-    { kanji: '⼽', stroke: '4', mean: ['spear', 'weapon'], reading: 'ほこ' },
-    { kanji: '⼾', stroke: '4', mean: ['door'], reading: 'と' },
-
-    { kanji: '⼿', stroke: '4', mean: ['hand'], reading: 'て' },
-    { kanji: '⽀', stroke: '4', mean: ['branch'], reading: 'しにょう' },
-    {
-        kanji: '⽁',
-        stroke: '4',
-        mean: ['activity', 'to strike', 'hit'],
-        reading: 'ぼくづくり',
-        other_reading: 'つくり'
-    },
-    {
-        kanji: '⺙',
-        stroke: '4',
-        mean: ['activity', 'to strike', 'hit'],
-        reading: 'ぼくづくり',
-        other_reading: 'つくり'
-    },
-    {
-        kanji: '⽂',
-        stroke: '4',
-        mean: ['literature', 'letters'],
-        reading: 'ぶん'
-    },
-    {
-        kanji: '⽃',
-        stroke: '4',
-        mean: ['big dipper', 'ladle', '18 liters'],
-        reading: 'ますづくり'
+const kanjicontainer = document.querySelector('.output-container');
+const Kanji = document.querySelector('#kanji');
+// Thêm class vào body
+const fetchData = async (Kanji, storyType) => {
+    try {
+        const response = await fetch(
+            `http://localhost:3000/api/v1/singleKanji/${Kanji}/${storyType}`
+        )
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
     }
-]
+}
 
-const kanjicontainer = document.querySelector('.output-kanji-up-right-mean')
-        const inputKanji = document.querySelector('#kanji')
-        const rendercmt = (input) => {
-            const filteredKanji = KANJI.filter(cmt => cmt.kanji === input)
-            const cmtHTML = filteredKanji.map(
-                (cmt) => `
-                    <p id="mean"> ${cmt.mean}</p>
-                    <p id="kun"> ${cmt.stroke}</p>
-                    <p id="on"> ${cmt.reading}</p>
-                    <p id="composition">${cmt.other_reading}</p>
-                `
-            )
-            kanjicontainer.innerHTML = cmtHTML.join('')
+
+btn.addEventListener("click", async function (){
+    try {
+        const data = await fetchData(Kanji.value, storyType)
+        let value = data.kanjiInfo
+        let kanji_list = Kanji.value;
+         const rendercmt = (input) => {
+            let kanjiHTMLarray = '';
+            
+           
+               value.forEach( 
+                    (cmt) => {
+                        const kanjiHtmlItem = `
+                        <div class="output">
+                        <div class="output-kanji">
+                            <div class="output-kanji-up">
+                                <div class="output-kanji-up-left">
+                                    <p id="output-kanji-ne" class="output-kanji-up-left-text">${cmt.kanji.character}</p>
+                                </div>
+                                <div class="output-kanji-up-right">
+                                    <div class="output-kanji-up-right-title">
+                                        <h3>Meaning:</h3>
+                                        <h3>Kunyomi:</h3>
+                                        <h3>Onyomi:</h3>
+                                        <h3>Radical:</h3>
+                                    </div>
+                                    <div class="output-kanji-up-right-mean">
+                                        <p id="mean"> ${cmt.kanji.meaning.english}</p>
+                                        <p id="kun"> ${cmt.kanji.kunyomi.hiragana}</p>
+                                        <p id="on"> ${cmt.kanji.onyomi.katakana}</p>
+                                        <p id="radical">${cmt.radical.character} (${cmt.radical.meaning.english})</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="output-kanji-down">
+                                    <p id="story">
+                                    ${cmt.story}
+                                    </p>
+                            </div>
+                        </div>
+                        <div class="save-reload">
+                            <i class="fa-regular fa-bookmark"></i>
+                            <i class="fa-solid fa-arrows-rotate"></i>
+                        </div>
+                    </div>
+                        `;
+                        kanjiHTMLarray +=`${kanjiHtmlItem} \n`;
+                    }
+                );
+
+                kanjicontainer.innerHTML = kanjiHTMLarray;
+
         }
+        rendercmt(kanji_list);
+    } catch (error) {
+        console.error(error)
+    }
+    
+})
 
-        let Kanji = document.querySelector('#kanji')
-        let output = document.querySelector('#output-kanji-ne')
-        let btn = document.querySelector('#create')
-        let output_frame = document.querySelector(".output")       
-
-        btn.addEventListener("click", function() {
-            event.preventDefault();
-            const inputVal = Kanji.value.trim(); // Trim to remove leading/trailing whitespaces
-            if (inputVal.length > 0) {
-                output_frame.style.display = "flex";
-                rendercmt(inputVal);
-            } else {
-                kanjicontainer.innerHTML = ''; // Clear container if input is empty
-            }
-            let input_kanji = Kanji.value;
-            output.innerText = input_kanji;
-        });
-
-        let tag_btn1 = document.querySelector(".normal")
-        let tag_btn2 = document.querySelector(".fun")
-        let tag_btn3 = document.querySelector(".drama")
-        let tag_btn4 = document.querySelector(".sad")
-
-        tag_btn1.addEventListener('click', () => {
-            tag_btn1.classList.add('active')
-            tag_btn2.classList.remove('active')
-            tag_btn3.classList.remove('active')
-            tag_btn4.classList.remove('active')
-        })
-
-        tag_btn2.addEventListener('click', () => {
-            tag_btn2.classList.add('active')
-            tag_btn1.classList.remove('active')
-            tag_btn3.classList.remove('active')
-            tag_btn4.classList.remove('active')
-        })
-
-        tag_btn3.addEventListener('click', () => {
-            tag_btn3.classList.add('active')
-            tag_btn2.classList.remove('active')
-            tag_btn1.classList.remove('active')
-            tag_btn4.classList.remove('active')
-        })
-
-        // tag_btn4.addEventListener('click', () => {
-        //     tag_btn4.classList.add('active')
-        //     tag_btn2.classList.remove('active')
-        //     tag_btn3.classList.remove('active')
-        //     tag_btn1.classList.remove('active')
-        // })
